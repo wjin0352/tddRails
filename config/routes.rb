4bd3c25e365e70a5da7244a650f62a2e5cf8ea54
won <wjin@nyit.edu>
2015-01-23 17:55:14 -0500

    'create a new project' feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+root "projects#index"
+
+resources :projects
+
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,16 +1,16 @@
-require 'spec_helper'
+require "rails_helper"
 
-RSpec.feature 'Creating Projects' do 
-	scenario "can create a project" do
-		visit '/'
+RSpec.feature "Creating Projects" do 
+	scenario "A user can create a new project" do
+		visit "/"
 
-		click_link 'New Project'
+		click_link "New Project"
 
-		fill_in 'Name', with: 'TextMate 2'
-		fill_in 'Description', with: 'A text-editor for OS X'
-		click_button 'Create Project'
+		fill_in "Name", with: "Sublime Text 3"
+		fill_in "Description", with: "A text-editor for everyone"
+		click_button "Create Project"
 
-		expect(page).to have_content('Project has been created.')
+		expect(page).to have_content("Project has been created.")
 	end
 end
 
