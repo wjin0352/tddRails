e3abc54df2d03f8fea5e6fcf5262f2610e8eaccb
won <wjin@nyit.edu>
2015-01-24 19:02:11 -0500

    projects can now be updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,23 @@ class ProjectsController < ApplicationController
 
 	def new 
 		@project = Project.new
-	end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
+
 
 	def create
 		@project = Project.new(project_params)
@@ -22,7 +38,8 @@ class ProjectsController < ApplicationController
       render "new"
 		end
 
-	end
+  end
+
 
 	private 
 
