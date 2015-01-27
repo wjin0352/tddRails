ccee78b6adf2bd1cba98d7424cbfb70daa986017
won <wjin@nyit.edu>
2015-01-27 01:53:46 -0500

    implemented creating tickets for project, with model controller tests and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
 root "projects#index"
 
-resources :projects
+resources :projects do
+  resources :tickets
+end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123180100) do
+ActiveRecord::Schema.define(version: 20150127003207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20150123180100) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "tickets", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
 
 end
