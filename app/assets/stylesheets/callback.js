0d7dd1380a1989dfbba4dd5ad65645d0a13308e8
won <wjin@nyit.edu>
2015-01-29 17:43:37 -0500

    stlyed sign up n sign in form

---
+++ app/assets/stylesheets/callback.js
@@ -0,0 +1,13 @@
+var maxTime = 1000;
+
+var evenDoubler = function(v, callback){
+	if (err) {
+		console.log("Error: "+ err.message);
+	} else {
+		console.log("the results are: "+ results + " (" + time + " ms)");
+	}
+};
+
+evenDoubler(2, handleResults);
+
+console.log("----");
